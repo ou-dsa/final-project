35be1c85000386c3b100922ce13c676b19bee7e5
Felipe Olivera <olivera.f94@gmail.com>
2017-12-11 20:21:05 -0600

    agg features code

--- Final.R
+++ Final.R
@@ -4,9 +4,8 @@ card.fraud <- read_csv("~/Documents/Ing.Informatica/Exchange/Intelligent Data An
                        col_types = cols(datetime = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
 
 # Time windows
-name <- c("12h", "1d", "2d", "7d", "30d")
-secs <- c(60 * 60 * 12,       # 12 hours
-          60 * 60 * 24,       # 1 day
+name <- c("1d", "2d", "7d", "30d")
+secs <- c(60 * 60 * 24,       # 1 day
           60 * 60 * 24 * 2,   # 2 days
           60 * 60 * 24 * 7,   # 7 days
           60 * 60 * 24 * 30)  # 30 days
@@ -40,5 +39,45 @@ for (i in 1:nrow(time.windows)) {
     }
   }
   
-  within(card.fraud, rm(Id))
+  card.fraud <- card.fraud[, !names(card.fraud) %in% c("Id")]
+}
+
+issuers <- unique(card.fraud$id_issuer)
+card.fraud[, "frd_by_id_issuer"] <- NA
+for (issuer in issuers) {
+  issuer.frauds <- nrow(card.fraud[card.fraud$is_fraud == 1 & card.fraud$id_issuer == issuer, ])
+  issuer.count <- nrow(card.fraud[card.fraud$id_issuer == issuer, ])
+  card.fraud$frd_by_id_issuer[card.fraud$id_issuer == issuer] <- issuer.frauds/issuer.count
+}
+
+merchants <- unique(card.fraud$id_merchant)
+card.fraud[, "frd_by_id_merchant"] <- NA
+for (merchant in merchants) {
+  merchant.frauds <- nrow(card.fraud[card.fraud$is_fraud == 1 & card.fraud$id_merchant == merchant, ])
+  merchant.count <- nrow(card.fraud[card.fraud$id_merchant == merchant, ])
+  card.fraud$frd_by_id_merchant[card.fraud$id_merchant == merchant] <- merchant.frauds/merchant.count
+}
+
+write.csv(card.fraud,
+          file = "~/Documents/Ing.Informatica/Exchange/Intelligent Data Analytics/Final/dataset_agg.csv",
+          row.names=FALSE)
+
+ComputeSavings <- function(amounts, pred.values, true.values) {
+  predictions <- data.frame(amounts, pred.values, true.values)
+  
+  costs <- 0
+  for (i in 1:nrow(predictions)) {
+    pred.value <- predictions$pred.values[i, ]
+    true.value <- predictions$true.values[i, ]
+    
+    if (pred.value == 1) {
+      costs <- costs + 20
+    } else if (pred.values == 0 & true.value == 1) {
+      costs <- costs + predictions$amount[i, ]
+    }
+  }
+  
+  savings <- sum(predictions$amounts[predictions$true.values == 1, ]) - costs
+  
+  return(savings)
 }
